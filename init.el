3cc1313479d4da570055a2834bda5acdc7456abb
Filip Górny <filip.gorny@gmail.com>
2016-05-04 17:52:26 +0200

    added rainbows and package management

--- init.el
+++ init.el
@@ -1,8 +1,34 @@
+;; packages
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+
+(defun require-package (package &optional min-version no-refresh)
+  "Install given PACKAGE, optionally requiring MIN-VERSION.
+If NO-REFRESH is non-nil, the available package lists will not be
+re-downloaded in order to locate PACKAGE."
+  (if (package-installed-p package min-version)
+          t
+      (if (or (assoc package package-archive-contents) no-refresh)
+              (package-install package)
+      (progn
+          (package-refresh-contents)
+          (require-package package min-version t)))))
+(package-initialize)
+
 ;; common system properties
 (setq backup-directory-alist
       `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
+
+;; libraries
+(add-to-list 'load-path "~/.emacs.d/vendor/")
+
 
 ;; common properties for gui
 (tool-bar-mode -1)
@@ -11,7 +37,7 @@
 (setq ring-bell-function 'ignore)
 (set-face-attribute 'fringe nil :background nil :foreground nil)
 ;; scroll one line at a time (less "jumpy" than defaults)
-(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
+(setq mouse-wheel-scroll-amount '(2 ((shift) . 2))) ;; one line at a time
 (setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
 (setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
 (setq scroll-step 1) ;; keyboard scroll one line at a time
@@ -21,7 +47,8 @@
 (load-theme 'natural-vibration t)
 (set-face-attribute 'default nil :font "Hack" :height 110)
 (set-face-attribute 'mode-line nil :box nil :height 110)
-(set-face-background 'mode-line "#333")
+(set-face-background 'mode-line "#223")
+(set-face-foreground 'mode-line "#769")
 (scroll-bar-mode -1)
 (set-cursor-color "#ff3311")
 (setq cursor-type '(bar . 6))
@@ -57,21 +84,28 @@
 
 (homerow-navigation-minor-mode 1)
 
+(global-set-key (kbd "M-RET") 'toggle-max-frame)
 
-;; packages
-(require 'package)
-(package-initialize)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
-
-;; libraries
-(add-to-list 'load-path "~/.emacs.d/vendor/")
-
-(load "sml-modeline.el")
-
+;; line numbers
 (require 'linum)
 (global-linum-mode)
 (setq linum-format " %d")
 (custom-set-variables '(linum-format (quote "%3d")))
-(custom-set-faces '(linum ((t (:foreground "#333" :background nil)))))
+(custom-set-faces '(linum ((t (:foreground "#335" :background nil)))))
 
+;; rainbow delimeters
+(require-package 'rainbow-delimiters)
+(require 'rainbow-delimiters)
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+(custom-set-faces
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "#ffaa00"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "#99ff99"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "#aa88ff"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "#88ffff"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "#88ff00"))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "#ffff66"))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "#44ff44"))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "#22ff22"))))
+ '(rainbow-delimiters-depth-9-face ((t (:foreground "#00ff00"))))
+ '(rainbow-delimiters-unmatched-face ((t (:foreground "red"))))
+ '(show-paren-match ((((class color) (background light)) (:background "azure2")))))
