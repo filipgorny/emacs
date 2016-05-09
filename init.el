41fce845c577453f940563e9174a6a39333642c1
Filip Górny <filip.gorny@gmail.com>
2016-05-09 23:22:17 +0200

    navigation between non special buffers

--- init.el
+++ init.el
@@ -17,7 +17,9 @@ re-downloaded in order to locate PACKAGE."
               (package-install package)
       (progn
           (package-refresh-contents)
-          (require-package package min-version t)))))
+          (require-package package min-version t))))
+  (require package))
+
 (package-initialize)
 
 ;; common system properties
@@ -26,9 +28,13 @@ re-downloaded in order to locate PACKAGE."
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
 
+;; empty scratch
+(setq initial-scratch-message "")
+
 ;; libraries
 (add-to-list 'load-path "~/.emacs.d/vendor/")
 
+(load "header")
 
 ;; common properties for gui
 (tool-bar-mode -1)
@@ -55,13 +61,36 @@ re-downloaded in order to locate PACKAGE."
 (set-face-attribute 'fringe nil
                       :foreground (face-foreground 'default)
                       :background (face-background 'default))
+
 ;; cua mode
 (cua-mode t)
 (setq cua-auto-tabify-rectangles nil) ;; Don't tabify after rectangle commands
 (transient-mark-mode 1) ;; No region when it is not highlighted
 (setq cua-keep-region-after-copy t) ;; Standard Windows behaviour
 
+;; autocomplete
+(require-package 'auto-complete)
+
 ;; common keybindings
+(defun jump-notspecial-buffer (direction)
+  (if (> direction 0)
+      (switch-to-next-buffer)
+    (switch-to-prev-buffer))
+  (while (string= (substring (buffer-name) 0 1) "*")
+    (if (> direction 0)
+	(switch-to-next-buffer)
+      (switch-to-prev-buffer)
+      ))
+  )
+
+(defun previous-notspecial-buffer ()
+  (jump-notspecial-buffer -1)
+  )
+
+(defun next-notspecial-buffer ()
+  (jump-notspecial-buffer 1)
+  )
+
 (global-set-key (kbd "C-d") 'kill-whole-line)
 (defvar homerow-navigation-minor-mode-map
     (let ((map (make-sparse-keymap)))
@@ -72,15 +101,15 @@ re-downloaded in order to locate PACKAGE."
         (define-key map (kbd "M-d") 'end-of-line)
         (define-key map (kbd "M-a") 'beginning-of-line)
         (define-key map (kbd "M-s") 'back-to-indentation)
-	(define-key map (kbd "M-=") 'next-buffer)
-	(define-key map (kbd "M--") 'previous-buffer)
+	(define-key map (kbd "M-=") 'next-notspecial-buffer)
+	(define-key map (kbd "M--") 'previous-notspecial-buffer)
         map)
     "f-navigation-minor-mode keymap.")
 
 (define-minor-mode homerow-navigation-minor-mode
     "A minor mode so that my key settings override annoying major modes."
     :init-value t
-    :lighter "f-navigation")
+    :lighter "homerow-navigation")
 
 (homerow-navigation-minor-mode 1)
 
@@ -91,16 +120,15 @@ re-downloaded in order to locate PACKAGE."
 (global-linum-mode)
 (setq linum-format " %d")
 (custom-set-variables '(linum-format (quote "%3d")))
-(custom-set-faces '(linum ((t (:foreground "#335" :background nil)))))
+(custom-set-faces '(linum ((t (:foreground "#223" :background nil)))))
 
 ;; rainbow delimeters
 (require-package 'rainbow-delimiters)
-(require 'rainbow-delimiters)
 (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
 (custom-set-faces
  '(rainbow-delimiters-depth-1-face ((t (:foreground "#ffaa00"))))
  '(rainbow-delimiters-depth-2-face ((t (:foreground "#99ff99"))))
- '(rainbow-delimiters-depth-3-face ((t (:foreground "#aa88ff"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foregrjjound "#aa88ff"))))
  '(rainbow-delimiters-depth-4-face ((t (:foreground "#88ffff"))))
  '(rainbow-delimiters-depth-5-face ((t (:foreground "#88ff00"))))
  '(rainbow-delimiters-depth-6-face ((t (:foreground "#ffff66"))))
@@ -109,3 +137,12 @@ re-downloaded in order to locate PACKAGE."
  '(rainbow-delimiters-depth-9-face ((t (:foreground "#00ff00"))))
  '(rainbow-delimiters-unmatched-face ((t (:foreground "red"))))
  '(show-paren-match ((((class color) (background light)) (:background "azure2")))))
+
+;; helm
+(require-package 'helm)
+(global-set-key (kbd "M-x") #'helm-M-x)
+(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
+(global-set-key (kbd "C-x C-f") #'helm-find-files)
+(helm-mode 1)
+
+
