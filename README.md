5f46ec806e1de15ec0f50e7da90faed06f6a33fa
Filip Górny <filip.gorny@gmail.com>
2016-10-09 13:24:11 +0200

    very basic setup

--- README.md
+++
@@ -1,3 +0,0 @@
-## Emacs <3
-This is my ~/.emacs.d/ that I use everyday for coding.
-
--- lisp/snippets.el
+++
@@ -1,6 +0,0 @@
-(require-package 'yasnippet)
-
-;;(add-to-list 'load-path
-;;              "~/.emacs.d/snippets")
-;;(require 'yasnippet)
-;;(yas-global-mode 1)
