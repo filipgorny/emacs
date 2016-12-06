403ae38bce66cb9d763da886bb7f40caefab9fe7
Filip Górny <filip.gorny@gmail.com>
2016-12-06 12:13:08 +0100

    theme and helm

--- lisp/line-numbers.el
+++ lisp/line-numbers.el
@@ -1,12 +1,13 @@
 (global-linum-mode 1)
 (custom-set-variables '(linum-format (quote " %3d ")))
-(set-face-foreground 'linum "#313131")
-(set-face-bold 'linum t)
+(set-face-foreground 'linum "#414141")
+(set-face-background 'linum "#191919")
+(set-face-bold 'linum nil)
 
 (require-package 'hlinum)
 (hlinum-activate)
 
 (setq linum-highlight-in-all-buffersp t)
 
-(set-face-background 'linum-highlight-face "#181818")
+(set-face-background 'linum-highlight-face "#222")
 (set-face-foreground 'linum-highlight-face "#aa9900")
